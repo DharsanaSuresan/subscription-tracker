ece164694290addb5fcb14d1a1a65b2fc6081908
DharsanaSuresan <155166113+DharsanaSuresan@users.noreply.github.com>
2026-03-14 09:39:11 +0530

    Refactor app.listen to include database connection

--- app.js
+++ app.js
@@ -22,8 +22,12 @@ app.get('/',(req,res)=>{
     res.send('Welcome to the Subscription Tracker API!');
 })
 
-app.listen(PORT,async()=>{
-    console.log(`Subscription Tracker API is running on http://localhost:${PORT}`);
+// app.listen(PORT,async()=>{
+//     console.log(`Subscription Tracker API is running on http://localhost:${PORT}`);
+//     await connectToDatabase();
+// });
+app.listen(PORT || 3000, async () => {
+    console.log(`Server running on port ${PORT || 3000}`);
     await connectToDatabase();
 });
 
